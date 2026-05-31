5a9beb1c8f27d06990770fafc38392da59348a61
Chris000jk <christianborges696@gmail.com>
2026-05-31 04:04:41 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -185,7 +185,7 @@ def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(catalogo, pattern="^catalogo$"))
-    app.add_handler(CallbackQueryHandler(comprar, pattern="^comprar_carro_hummer$"))
+    app.add_handler(CallbackQueryHandler(comprar, pattern="comprar_carro_hummer"))
     app.add_handler(CallbackQueryHandler(verificar, pattern="^verificar$"))
     
     print("🚀 Bot corriendo con galería de 4 fotos...")
